9b64be54d4ebf80c992c508c1315a32dc2a0de96
명은 권 <dksrltpdud@gmail.com>
2019-06-22 11:04:50 +0900

    포켓몬

--- MoonMyeong/MoonMyeong/Form1.vb
+++ MoonMyeong/MoonMyeong/Form1.vb
@@ -1,27 +1,32 @@
 ﻿Public Class Form1
 
     Private Sub Button1_MouseMove(sender As Object, e As MouseEventArgs) Handles Button1.MouseMove
-        flower1.ImageLocation = "C:\Users\android\Source\Repos\Moonheenam\Megumi\MoonMyeong\MoonMyeong\Image\꽃남.jpg"
+        flower1.ImageLocation = "..\..\Image\꽃남.jpg"
     End Sub
 
     Private Sub Button1_MouseLeave(sender As Object, e As EventArgs) Handles Button1.MouseLeave
-        flower1.ImageLocation = "C:\Users\android\Source\Repos\Moonheenam\Megumi\MoonMyeong\MoonMyeong\Image\수줍남.PNG"
+        flower1.ImageLocation = "..\..\Image\수줍남.PNG"
     End Sub
 
     Private Sub Button2_MouseMove(sender As Object, e As MouseEventArgs) Handles Button2.MouseMove
-        flower2.ImageLocation = "C:\Users\android\Source\Repos\Moonheenam\Megumi\MoonMyeong\MoonMyeong\Image\꽃남.jpg"
+        flower2.ImageLocation = "..\..\Image\꽃남.jpg"
     End Sub
 
     Private Sub Button2_MouseLeave(sender As Object, e As EventArgs) Handles Button2.MouseLeave
-        flower2.ImageLocation = "C:\Users\android\Source\Repos\Moonheenam\Megumi\MoonMyeong\MoonMyeong\Image\수줍남.PNG"
+        flower2.ImageLocation = "..\..\Image\수줍남.PNG"
     End Sub
 
     Private Sub Button3_MouseMove(sender As Object, e As MouseEventArgs) Handles Button3.MouseMove
-        flower3.ImageLocation = "C:\Users\android\Source\Repos\Moonheenam\Megumi\MoonMyeong\MoonMyeong\Image\꽃남.jpg"
+        flower3.ImageLocation = "..\..\Image\꽃남.jpg"
     End Sub
 
     Private Sub Button3_MouseLeave(sender As Object, e As EventArgs) Handles Button3.MouseLeave
-        flower3.ImageLocation = "C:\Users\android\Source\Repos\Moonheenam\Megumi\MoonMyeong\MoonMyeong\Image\수줍남.PNG"
+        flower3.ImageLocation = "..\..\Image\수줍남.PNG"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
     End Sub
     '
+
 End Class
